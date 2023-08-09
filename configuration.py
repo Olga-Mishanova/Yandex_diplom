bed6aba27c1cf4c3ec13d3213062af2d7fc96644
Ольга Мишанова <olga_mishanova@MacBook-Air-Olga.local>
2023-08-09 10:12:31 +0300

    My first commit of diplom yandex

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL = 'https://dcc8d709-62db-43aa-92c5-d70a54667945.serverhub.praktikum-services.ru'
+CREATE_NEW_ORDER_PATH = '/api/v1/orders/'
+GET_ORDER_PATH = '/api/v1/orders/track/'
---
+++ data.py
@@ -0,0 +1,18 @@
+
+headers = {'Content-Type': 'application/json'}
+
+body_order = {
+    'firstName': 'Ольга',
+    'lastName': 'Иванова',
+    'address': 'Семенова 7',
+    'metroStation': '15',
+    'phone': '+79062345678',
+    'rentTime': 2,
+    'deliveryDate': '2023-08-15',
+    'comment': 'Привет, Иван!',
+    'color': [
+        "BLACK"
+    ]
+}
+
+param = {}
